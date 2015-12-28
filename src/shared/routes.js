cbd809371160128d572b52f175cc42b555f72aaf
nirvan <nirvanyamil@gmail.com>
2015-12-28 14:14:47 -0800

    contact

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -5,7 +5,7 @@ import { IndexRoute, Router, Route, Link } from 'react-router';
 import AppHandler from "./components/AppHandler";
 import HomeSection from "./components/sections/home";
 import AboutUsSection from "./components/sections/aboutus";
-import ContactSection from "./components/sections/contact";
+import ContactSection from "./components/sections/contact/index_n";
 
 
 export default(
