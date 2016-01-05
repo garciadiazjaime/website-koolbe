6ae3ee48b4820fb8eb8d2381c2d90ee41b95716a
Jaime Garcia <jgarciaqo@gmail.com>
2016-01-05 14:34:50 -0600

    init footer

---
+++ src/shared/components/layout/footer/footerAAA/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React, { Component, PropTypes} from 'react';
+import _ from 'lodash';
+
+
+export default class FooterAAA extends Component {
+
+
+  render() {
+
+    return (<div>
+        <div className="container">footer</div>
+      </div>
+    );
+  }
+}
