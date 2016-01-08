52cdd0c6ef756f4321494679414460ebeca98769
Jaime Garcia <jgarciaqo@gmail.com>
2016-01-07 20:48:34 -0600

    contact send

--- src/shared/components/sections/contact/form.js
+++ src/shared/components/sections/contact/form.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from 'react-router';
 
 var style = process.env.NODE_ENV === 'DEV' ? require("./style.scss") : {};
-import Intro from './intro';
-
+import RestClient from '../../../../server/lib/rest-client';
+var constants = require('../../../constants');
 
 export default class Form extends React.Component {
 
@@ -12,10 +12,11 @@ export default class Form extends React.Component {
     this.state = {
       formData: this.getInitialFormState(),
       requiredFields: this.getRequiredFields(this.getInitialFormState()),
-      showLoading: false,
-      errorCSSClass: 'text-error',
-      successCSSClass: 'text-success'
+      showLoading: false
     };
+
+    this.submitFormHandler = this.submitFormHandler.bind(this);
+    this.onChangeHandler = this.onChangeHandler.bind(this);
   }
 
   render() {
@@ -24,7 +25,7 @@ export default class Form extends React.Component {
         <div className="form-group">
           <label id="lab_name" className="col-sm-2 control-label">Nombre</label>
           <div className="col-sm-10">
-            <input type="text" id="name" name="name" placeholder="" onChange={this.onChangeHandler.bind(this, 'name')} value={this.state.formData.name.value} />
+            <input type="text" id="name" name="name" placeholder="" onChange={this.onChangeHandler} value={this.state.formData.name.value} />
           </div>
           <div className={style.borderBottom}></div>
         </div>
@@ -32,7 +33,7 @@ export default class Form extends React.Component {
         <div className="form-group">
           <label id="lab_email" className="col-sm-2 control-label">Correo</label>
           <div className="col-sm-10">
-            <input type="text" id="email" name="email" placeholder="" onChange={this.onChangeHandler.bind(this, 'email')} value={this.state.formData.email.value}/>
+            <input type="text" id="email" name="email" placeholder="" onChange={this.onChangeHandler} value={this.state.formData.email.value}/>
           </div>
           <div className={style.borderBottom}></div>
         </div>
@@ -40,7 +41,7 @@ export default class Form extends React.Component {
         <div className="form-group">
           <label id="lab_tel" className="col-sm-2 control-label">Teléfono</label>
           <div className="col-sm-10">
-            <input type="tel" id="tel" name="tel" placeholder="" onChange={this.onChangeHandler.bind(this, 'tel')} value={this.state.formData.tel.value}/>
+            <input type="tel" id="tel" name="tel" placeholder="" onChange={this.onChangeHandler} value={this.state.formData.tel.value}/>
           </div>
           <div className={style.borderBottom}></div>
         </div>
@@ -48,13 +49,13 @@ export default class Form extends React.Component {
         <div className="form-group">
           <label id="lab_message" className="col-sm-2 control-label">Mensaje</label>
           <div className="col-sm-10">
-            <textarea id="message" name="message" className="form-control" onChange={this.onChangeHandler.bind(this, 'message')} value={this.state.formData.message.value}></textarea>
+            <textarea id="message" name="message" className="form-control" onChange={this.onChangeHandler} value={this.state.formData.message.value}></textarea>
           </div>
           <div className={style.borderBottom}></div>
         </div>
 
         <div><span id="msg"></span></div>
-        <div>{ this.state.showLoading ? <span id="loader">Cargando</span> : null }</div>
+        <div>{ this.state.showLoading ? <span className={style.loader}>Cargando</span> : null }</div>
         <div className="pull-right">
           <button onClick={this.submitFormHandler}>Enviar</button>
         </div>
@@ -74,10 +75,6 @@ export default class Form extends React.Component {
         value: '',
         require: true
       },
-      company: {
-        title: 'Empresa',
-        value: ''
-      },
       tel: {
         title: 'Teléfono',
         value: '',
@@ -85,15 +82,15 @@ export default class Form extends React.Component {
       },
       message: {
         title: 'Mensaje',
-        value: ''
+        value: '',
+        require: true
       }
     };
   }
 
-  onChangeHandler() {
+  onChangeHandler(event) {
     var formData = this.state.formData;
-    formData[property].value = event.target.value;
-
+    formData[event.target.name].value = event.target.value;
     this.setState({
       formData: formData
     });
@@ -106,69 +103,70 @@ export default class Form extends React.Component {
     event.preventDefault();
     var formData = this.state.formData;
     var isFormValid = this.validateForm(formData, this.state.requiredFields);
-    var msgElement = document.getElementById('msg');
+    var msgElement = $('#msg');
+    msgElement.removeClass(style.errorCSSClass + ' ' + style.successCSSClass);
 
-    if(isFormValid){
-    this.setState({
-    showLoading: false
-    });
+    if(isFormValid) {
+      this.setState({
+        showLoading: true
+      });
 
-    var _this = this;
-    var html = this.getHTMLMessage(formData);
-    var data = {
-    fromname: formData.name.value,
-    replyto: formData.email.value,
-    subject: 'Forma de Contacto Web',
-    html: html
-    };
+      var _this = this;
+      var html = this.getHTMLMessage(formData);
+      var data = {
+        fromname: formData.name.value,
+        replyto: formData.email.value,
+        subject: constants.emailSubject,
+        html: html
+      };
 
-    RestClient({
-    path: '/api/send_email',
-    method: 'POST',
-    entity: data
-    }).then(function(response) {
-    var state = {
-    showLoading: false
-    };
-    if(response.entity.status){
-    state['formData'] = _this.getInitialFormState();
-    }
-    _this.setState(state);
-    msgElement.className = response.entity.status ? _this.state.successCSSClass : _this.state.errorCSSClass;
-    msgElement.innerHTML = response.entity.status ?
-    'Your information has been sent successfully, thanks.':
-    'We are sorry, something went wrong, please try later.';
-    });
+      RestClient({
+        path: '/api/send_email',
+        method: 'POST',
+        entity: data
+      }).then(function(response) {
+        var state = {
+          showLoading: false
+        };
+        if(response.entity.status){
+          state['formData'] = _this.getInitialFormState();
+        }
+        _this.setState(state);
+        msgElement.addClass(response.entity.status ? style.successCSSClass :style.errorCSSClass);
+        msgElement.html(response.entity.status ?
+        'Información enviada de manera exitosa, gracias.':
+        'Lo sentimos, favor de intentar más tarde.');
+      });
     }else{
-    msgElement.className = this.state.errorCSSClass;
+      msgElement.addClass(style.errorCSSClass);
     }
-    msgElement.innerHTML = !isFormValid ? 'Please fill in fields on red' : '';
+    msgElement.html(!isFormValid ? 'Favor de llenar los campos en rojo.' : '');
   }
 
   getRequiredFields(data) {
     var response = [];
     for(var property in data){
-    if(data[property].require){
-    response.push(property);
-    }
+      if(data[property].require){
+        response.push(property);
+      }
     }
     return response;
   }
 
   validateForm(data, requiredFields) {
     var response = true;
-    for(var i=0; i<requiredFields.length; i++){
-    var property = requiredFields[i];
-    var labelElement = document.getElementById('lab_' + property);
-    if( !data.hasOwnProperty(property) || !data[property].value.length){
-    if(response){
-    response = false;
-    }
-    labelElement.className = this.state.errorCSSClass;
-    }
-    else{
-    labelElement.className = '';
-    }
+    for(var i = 0, len = requiredFields.length; i<len; i++) {
+      var property = requiredFields[i];
+      var labelElement = $('#lab_' + property);
+      if( !data.hasOwnProperty(property) || !data[property].value.length){
+        if(response){
+          response = false;
+        }
+        labelElement.addClass(style.errorCSSClass);
+      }
+      else{
+        labelElement.removeClass(style.errorCSSClass);
+      }
     }
     return response;
   }
@@ -176,170 +174,8 @@ export default class Form extends React.Component {
   getHTMLMessage(data) {
     var response = '';
     for(var property in data){
-    response += data[property].title + ': ' + data[property].value + '<br />'
+      response += data[property].title + ': ' + data[property].value + '<br />'
     }
     return response;
   }
 }
-
-// var ContactForm = React.createClass({
-//
-//     getInitialState: function(){
-//         return {
-//             formData: this.getInitialFormState(),
-//             requiredFields: this.getRequiredFields(this.getInitialFormState()),
-//             showLoading: false,
-//             errorCSSClass: 'text-error',
-//             successCSSClass: 'text-success'
-//         };
-//     },
-//
-//     render: function() {
-//
-//         return (
-//             <form id="form">
-//                 <h2>Drop Us a Line</h2>
-//                 <p>Feel free to reach out to us with any questions.</p>
-//                 <div className="formGroup">
-//                     <label id="lab_name">Name*</label>
-//                     <input type="text" id="name" name="name" placeholder="Name" onChange={this.onChangeHandler.bind(this, 'name')} value={this.state.formData.name.value}/>
-//                 </div>
-//                 <div className="formGroup">
-//                     <label id="lab_email">Email*</label>
-//                     <input type="text" id="email" name="email" placeholder="Email" onChange={this.onChangeHandler.bind(this, 'email')} value={this.state.formData.email.value}/>
-//                 </div>
-//                 <div className="formGroup">
-//                     <label id="lab_tel">Phone*</label>
-//                     <input type="tel" id="tel" name="tel" placeholder="Phone" onChange={this.onChangeHandler.bind(this, 'tel')} value={this.state.formData.tel.value}/>
-//                 </div>
-//                 <textarea id="message" name="message" placeholder="Message" onChange={this.onChangeHandler.bind(this, 'message')} value={this.state.formData.message.value}></textarea>
-//                 <span id="msg"></span>
-//
-//                 { this.state.showLoading ? <span id="loader">Cargando</span> : null }
-//
-//                 <button id="submitButton" onClick={this.submitFormHandler}><span>Send</span></button>
-//             </form>
-//         )
-//     },
-//
-//     onChangeHandler: function(property, event){
-//         var formData = this.state.formData;
-//         formData[property].value = event.target.value;
-//
-//         this.setState({
-//             formData: formData
-//         });
-//     },
-//
-//     /*
-//         Handler function to validate form and send data
-//     */
-//     submitFormHandler: function(event){
-//         event.preventDefault();
-//         var formData = this.state.formData;
-//         var isFormValid = this.validateForm(formData, this.state.requiredFields);
-//         var msgElement = document.getElementById('msg');
-//
-//         if(isFormValid){
-//             this.setState({
-//                 showLoading: false
-//             });
-//
-//             var _this = this;
-//             var html = this.getHTMLMessage(formData);
-//             var data = {
-//                 fromname: formData.name.value,
-//                 replyto: formData.email.value,
-//                 subject: 'Forma de Contacto Web',
-//                 html: html
-//             };
-//
-//             RestClient({
-//                 path: '/api/send_email',
-//                 method: 'POST',
-//                 entity: data
-//             }).then(function(response) {
-//                 var state = {
-//                     showLoading: false
-//                 };
-//                 if(response.entity.status){
-//                     state['formData'] = _this.getInitialFormState();
-//                 }
-//                 _this.setState(state);
-//                 msgElement.className = response.entity.status ? _this.state.successCSSClass : _this.state.errorCSSClass;
-//                 msgElement.innerHTML = response.entity.status ?
-//                     'Your information has been sent successfully, thanks.':
-//                     'We are sorry, something went wrong, please try later.';
-//             });
-//         }else{
-//             msgElement.className = this.state.errorCSSClass;
-//         }
-//         msgElement.innerHTML = !isFormValid ? 'Please fill in fields on red' : '';
-//     },
-//
-//     getRequiredFields: function(data){
-//         var response = [];
-//         for(var property in data){
-//             if(data[property].require){
-//                 response.push(property);
-//             }
-//         }
-//         return response;
-//     },
-//
-//     validateForm: function(data, requiredFields){
-//         var response = true;
-//         for(var i=0; i<requiredFields.length; i++){
-//             var property = requiredFields[i];
-//             var labelElement = document.getElementById('lab_' + property);
-//             if( !data.hasOwnProperty(property) || !data[property].value.length){
-//                 if(response){
-//                     response = false;
-//                 }
-//                 labelElement.className = this.state.errorCSSClass;
-//             }
-//             else{
-//                 labelElement.className = '';
-//             }
-//         }
-//         return response;
-//     },
-//
-//     getHTMLMessage: function(data){
-//         var response = '';
-//         for(var property in data){
-//             response += data[property].title + ': ' + data[property].value + '<br />'
-//         }
-//         return response;
-//     },
-//
-//     getInitialFormState: function(){
-//         return {
-//             name: {
-//                 title: 'Nombre',
-//                 value: '',
-//                 require: true
-//             },
-//             email:{
-//                 title: 'Correo',
-//                 value: '',
-//                 require: true
-//             },
-//             company:{
-//                 title: 'Empresa',
-//                 value: ''
-//             },
-//             tel: {
-//                 title: 'Teléfono',
-//                 value: '',
-//                 require: true
-//             },
-//             message: {
-//                 title: 'Mensaje',
-//                 value: ''
-//             }
-//         };
-//     }
-// });
-//
-// module.exports = ContactForm;
